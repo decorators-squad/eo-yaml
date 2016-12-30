6a6cae30a9749ab2c0cd2578b81e79dde03e15a7
amihaiemil <amihaiemil@gmail.com>
2016-12-30 19:06:10 +0200

    AbstractNode, Scalar and unit tests

---
+++ src/main/java/com/amihaiemil/camel/AbstractNode.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2016, Mihai Emil Andronache
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package com.amihaiemil.camel;
+
+import java.util.Collection;
+
+/**
+ * YAML node.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 1.0.0
+ * @see http://yaml.org/spec/1.2/spec.html#node//
+ * @todo #7:1h/DEV Implement and unit test Sequence node.
+ *  See specification: http://yaml.org/spec/1.2/spec.html#sequence//
+ * @todo #7:1h/DEV Implement and unit test Mapping node.
+ *  See specification: http://yaml.org/spec/1.2/spec.html#mapping//
+ */
+abstract class AbstractNode {
+
+    /**
+     * Parents of this node.
+     */
+    private Collection<AbstractNode> parents;
+    
+    /**
+     * Children of this node.
+     */
+    private Collection<AbstractNode> children;
+
+    /**
+     * Ctor.
+     * @param parents Given parents
+     * @param children Given children
+     */
+    AbstractNode(
+        final Collection<AbstractNode> parents,
+        final Collection<AbstractNode> children
+    ) {
+        if(parents.isEmpty()) {
+            throw new IllegalStateException(
+                "A YAML graph cannot have orphaned nodes"
+            );
+        }
+        this.parents = parents;
+        this.children = children;
+    }
+    
+    /**
+     * Fetch the parent nodes of this node.
+     * @return Collection of {@link AbstractNode}
+     */
+    public Collection<AbstractNode> parents() {
+        return this.parents;
+    }
+    
+    /**
+     * Fetch the child nodes of this node.
+     * @return Collection of {@link AbstractNode}
+     */
+    public Collection<AbstractNode> children() {
+        return this.parents;
+    }
+
+}
---
+++ src/test/java/com/amihaiemil/camel/ScalarTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2016, Mihai Emil Andronache
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package com.amihaiemil.camel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Unit tests for {@link Scalar}
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 1.0.0
+ *
+ */
+public final class ScalarTest {
+
+    /**
+     * Scalar can return its own value.
+     */
+    @Test
+    public void returnsValue() {
+        final String val = "test scalar value";
+        final Scalar<String> scl = new Scalar<String>(
+            Arrays.asList(Mockito.mock(AbstractNode.class)),
+            val
+        );
+        MatcherAssert.assertThat(
+            scl.value(), Matchers.equalTo(val)
+        );
+    }
+    
+    /**
+     * Scalar throws ISE if no parents are specified.
+     */
+    @Test (expected = IllegalStateException.class)
+    public void orphanForbidden() {
+        new Scalar<String>(
+            new ArrayList<AbstractNode>(), "orphan"
+        );
+    }
+}
