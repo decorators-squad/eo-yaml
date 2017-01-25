3a1b12b9773f44bde882460a02551ed75eed7f6e
sherif <sherifwaly95@gmail.com>
2017-01-25 22:00:38 +0200

    Added methods and puzzles for YamlSerializer

--- src/main/java/com/amihaiemil/camel/YamlSerializer.java
+++ src/main/java/com/amihaiemil/camel/YamlSerializer.java
@@ -1,5 +1,40 @@
 package com.amihaiemil.camel;
 
-public interface YamlSerializer {
+import java.util.Collection;
+import java.util.Map;
 
+/**
+ * A Yaml serializer.
+ * @author sherif
+ * @version $Id$
+ * @param <YamlNode>
+ * @since 1.0.0
+ */
+public interface YamlSerializer<YamlNode> {
+    
+    /**
+     * Serialize a Map<Object, Object> to YamlNode.
+     * @param map Map<Object, Object>
+     * @return Yaml node
+     * @TODO #30 Implement the serialize method with Map input and unit-test it.
+     */
+    YamlNode serialize(final Map<Object, Object> map);
+    
+    /**
+     * Serialize a Map<Object, Object> to YamlNode.
+     * @param object Object
+     * @return Yaml node
+     * @TODO #30 Implement the serialize method with Object input
+     * and unit-test it.
+     */
+    YamlNode serialize(final Object object);
+    
+    /**
+     * Serialize a Map<Object, Object> to YamlNode.
+     * @param collection Collection<Object>
+     * @return Yaml node
+     * @TODO #30 Implement the serialize method with Collection input 
+     * and unit-test it.
+     */
+    YamlNode serialize(final Collection<Object> collection);
 }
