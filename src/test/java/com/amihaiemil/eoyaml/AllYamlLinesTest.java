296a5e75c8fccac5db779f7b5dd6ce2b7c836395
amihaiemil <amihaiemil@gmail.com>
2020-03-24 19:46:20 +0200

    unit tests for AllYamlLines.toYamlNode()

--- src/test/java/com/amihaiemil/eoyaml/AllYamlLinesTest.java
+++ src/test/java/com/amihaiemil/eoyaml/AllYamlLinesTest.java
@@ -28,11 +28,12 @@
 package com.amihaiemil.eoyaml;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -95,4 +96,188 @@ public final class AllYamlLinesTest {
         MatcherAssert.assertThat(iterator.hasNext(), Matchers.is(false));
     }
 
+    /**
+     * Unit test for AllYamlLines.toYamlNode(). The lines should turn into a
+     * literal block scalar.
+     */
+    @Test
+    public void turnsIntoLiteralBlockScalar() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("line1", 1));
+        lines.add(new RtYamlLine("line2", 2));
+        lines.add(new RtYamlLine("line3", 3));
+        final YamlLines yamlLines = new AllYamlLines(lines);
+        MatcherAssert.assertThat(
+            yamlLines.toYamlNode(new RtYamlLine("literalScalar:|", 0)),
+            Matchers.instanceOf(ReadLiteralBlockScalar.class)
+        );
+    }
+
+    /**
+     * Unit test for AllYamlLines.toYamlNode(). The lines should turn into a
+     * folded block scalar.
+     */
+    @Test
+    public void turnsIntoFoldedBlockScalar() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("more lines for", 1));
+        lines.add(new RtYamlLine("better understanding", 2));
+        lines.add(new RtYamlLine("of text", 3));
+        final YamlLines yamlLines = new AllYamlLines(lines);
+        MatcherAssert.assertThat(
+            yamlLines.toYamlNode(new RtYamlLine("foldedScalar:>", 0)),
+            Matchers.instanceOf(ReadFoldedBlockScalar.class)
+        );
+    }
+
+    /**
+     * Unit test for AllYamlLines.toYamlNode(). The lines should turn into a
+     * (folded) sequence.
+     */
+    @Test
+    public void turnsIntoFoldedSequence() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("value1", 1));
+        lines.add(new RtYamlLine("value2", 2));
+        lines.add(new RtYamlLine("value3", 3));
+        final YamlLines yamlLines = new AllYamlLines(lines);
+        final YamlNode seq =  yamlLines.toYamlNode(
+            new RtYamlLine("foldedSequence:|-", 0)
+        );
+        MatcherAssert.assertThat(
+            seq, Matchers.instanceOf(ReadYamlSequence.class)
+        );
+        final Collection<YamlNode> values = ((YamlSequence) seq).values();
+        MatcherAssert.assertThat(values.size(), Matchers.is(3));
+        System.out.print(values);
+        MatcherAssert.assertThat(
+            values.contains(new BuiltPlainScalar("value1")),
+            Matchers.is(Boolean.TRUE)
+        );
+        MatcherAssert.assertThat(
+            values.contains(
+                new BuiltPlainScalar("value2")
+            ),
+            Matchers.is(Boolean.TRUE)
+        );
+        MatcherAssert.assertThat(
+            values.contains(new BuiltPlainScalar("value3")),
+            Matchers.is(Boolean.TRUE)
+        );
+    }
+
+    /**
+     * Unit test for AllYamlLines.toYamlNode(). The lines should turn into a
+     * (folded) sequence. There are spaces between the last 2 characters of
+     * the previous line.
+     */
+    @Test
+    public void turnsIntoFoldedSequenceWithSpaces() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("value1", 1));
+        lines.add(new RtYamlLine("value2", 2));
+        lines.add(new RtYamlLine("value3", 3));
+        final YamlLines yamlLines = new AllYamlLines(lines);
+        final YamlNode seq =  yamlLines.toYamlNode(
+            new RtYamlLine("foldedSequence:| -", 0)
+        );
+        MatcherAssert.assertThat(
+            seq, Matchers.instanceOf(ReadYamlSequence.class)
+        );
+        final Collection<YamlNode> values = ((YamlSequence) seq).values();
+        MatcherAssert.assertThat(values.size(), Matchers.is(3));
+        System.out.print(values);
+        MatcherAssert.assertThat(
+            values.contains(new BuiltPlainScalar("value1")),
+            Matchers.is(Boolean.TRUE)
+        );
+        MatcherAssert.assertThat(
+            values.contains(
+                new BuiltPlainScalar("value2")
+            ),
+            Matchers.is(Boolean.TRUE)
+        );
+        MatcherAssert.assertThat(
+            values.contains(new BuiltPlainScalar("value3")),
+            Matchers.is(Boolean.TRUE)
+        );
+    }
+
+    /**
+     * Unit test for AllYamlLines.toYamlNode(). The lines should turn
+     * into a (detected) mapping.
+     */
+    @Test
+    public void turnsIntoMapping() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("some: mapping", 1));
+        lines.add(new RtYamlLine("for: test", 2));
+        final YamlLines yamlLines = new AllYamlLines(lines);
+        MatcherAssert.assertThat(
+            yamlLines.toYamlNode(new RtYamlLine("---", 0)),
+            Matchers.instanceOf(ReadYamlMapping.class)
+        );
+    }
+
+    /**
+     * Unit test for AllYamlLines.toYamlNode(). The lines should turn
+     * into a (detected) sequence.
+     */
+    @Test
+    public void turnsIntoSequence() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("- some", 1));
+        lines.add(new RtYamlLine("- sequence", 2));
+        final YamlLines yamlLines = new AllYamlLines(lines);
+        MatcherAssert.assertThat(
+            yamlLines.toYamlNode(new RtYamlLine("?", 0)),
+            Matchers.instanceOf(ReadYamlSequence.class)
+        );
+    }
+
+    /**
+     * Unit test for AllYamlLines.toYamlNode(). The lines should turn
+     * into a (detected) sequence.
+     */
+    @Test
+    public void turnsIntoPlainScalar() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("justAScalar", 1));
+        final YamlLines yamlLines = new AllYamlLines(lines);
+        MatcherAssert.assertThat(
+            yamlLines.toYamlNode(new RtYamlLine("---", 0)),
+            Matchers.instanceOf(ReadPlainScalarValue.class)
+        );
+    }
+
+    /**
+     * Unit test for AllYamlLines.toYamlNode(). It should complain, because
+     * it cannot detect the type of node. The previous line does not point to
+     * any specific node and the lines themselves are not a mapping, sequence
+     * or a plains scalar.
+     */
+    @Test
+    public void complainsWhenCannotTurn() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("this will fail", 0));
+        lines.add(new RtYamlLine("because these lines", 1));
+        lines.add(new RtYamlLine("have no YAML meaning.", 2));
+        lines.add(new RtYamlLine("They are also not", 3));
+        lines.add(new RtYamlLine("a folded or literal scalar", 4));
+        final YamlLines yamlLines = new AllYamlLines(lines);
+        try {
+            yamlLines.toYamlNode(new RtYamlLine("---", -1));
+            Assert.fail("Expected IllegalStateException!");
+        } catch (final IllegalStateException ex) {
+            final String message = ex.getMessage();
+            MatcherAssert.assertThat(
+                message,
+                Matchers.startsWith("Could not parse YAML starting at line 1")
+            );
+            MatcherAssert.assertThat(
+                message,
+                Matchers.containsString("but it has 5 lines")
+            );
+        }
+    }
 }
