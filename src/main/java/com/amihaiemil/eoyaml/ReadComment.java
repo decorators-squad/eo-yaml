3bbfe1a0edf4176f29f8b8d57e92bdd6fa62594a
amihaiemil <amihaiemil@gmail.com>
2020-04-04 11:31:17 +0300

    #272 ReadComment and ReadCommentTest

--- src/main/java/com/amihaiemil/eoyaml/ReadComment.java
+++ src/main/java/com/amihaiemil/eoyaml/ReadComment.java
@@ -1,4 +1,72 @@
+/**
+ * Copyright (c) 2016-2020, Mihai Emil Andronache
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 package com.amihaiemil.eoyaml;
 
-public class ReadComment {
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * A comment which has been read from somewhere.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since  4.2.0
+ */
+final class ReadComment implements Comment {
+
+    /**
+     * Lines of this comment.
+     */
+    private final List<YamlLine> lines;
+
+    /**
+     * Node to which this comment refers.
+     */
+    private final YamlNode node;
+
+    /**
+     * Constructor.
+     * @param lines Lines of this comment.
+     * @param node Node to which it refers.
+     */
+    ReadComment(final List<YamlLine> lines, final YamlNode node) {
+        this.lines = lines;
+        this.node = node;
+    }
+
+    @Override
+    public YamlNode yamlNode() {
+        return this.node;
+    }
+
+    @Override
+    public String value() {
+        return this.lines.stream().map(
+            line -> line.trimmed().substring(1).trim()
+        ).collect(Collectors.joining(System.lineSeparator()));
+    }
 }
--- src/test/java/com/amihaiemil/eoyaml/ReadCommentTest.java
+++ src/test/java/com/amihaiemil/eoyaml/ReadCommentTest.java
@@ -1,4 +1,157 @@
+/**
+ * Copyright (c) 2016-2020, Mihai Emil Andronache
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 package com.amihaiemil.eoyaml;
 
-public class ReadCommentTest {
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit tests for {@link ReadComment}.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 4.2.0
+ */
+public final class ReadCommentTest {
+
+    /**
+     * ReadComment can return the YamlNode that it refers to.
+     */
+    @Test
+    public void returnsYamlNode() {
+        final YamlNode node = Mockito.mock(YamlNode.class);
+        MatcherAssert.assertThat(
+            new ReadComment(new ArrayList<>(), node).yamlNode(),
+            Matchers.is(node)
+        );
+    }
+
+    /**
+     * ReadComment can return the empty comment from empty yaml lines.
+     */
+    @Test
+    public void returnsValueFromEmptyLines() {
+        MatcherAssert.assertThat(
+            new ReadComment(
+                new ArrayList<>(),
+                Mockito.mock(YamlNode.class)
+            ).value(),
+            Matchers.isEmptyString()
+        );
+    }
+
+    /**
+     * ReadComment can return the comment from a single YamlLine.
+     */
+    @Test
+    public void returnsValueFromSingleLine() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("# comment line", 0));
+        MatcherAssert.assertThat(
+            new ReadComment(
+                lines,
+                Mockito.mock(YamlNode.class)
+            ).value(),
+            Matchers.equalTo("comment line")
+        );
+    }
+
+    /**
+     * ReadComment can return a comment spanning multiple lines.
+     */
+    @Test
+    public void returnsMultiLineComment() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("# comment", 0));
+        lines.add(new RtYamlLine("# on multiple", 1));
+        lines.add(new RtYamlLine("# lines", 2));
+        final StringBuilder expected = new StringBuilder();
+        expected
+            .append("comment").append(System.lineSeparator())
+            .append("on multiple").append(System.lineSeparator())
+            .append("lines");
+        MatcherAssert.assertThat(
+            new ReadComment(
+                lines,
+                Mockito.mock(YamlNode.class)
+            ).value(),
+            Matchers.equalTo(expected.toString())
+        );
+    }
+
+    /**
+     * ReadComment can return a comment spanning multiple lines, that
+     * also has empty lines.
+     */
+    @Test
+    public void returnsMultiLineCommentWithEmptyLines() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("# comment", 0));
+        lines.add(new RtYamlLine("# ", 1));
+        lines.add(new RtYamlLine("# on multiple", 2));
+        lines.add(new RtYamlLine("# ", 2));
+        lines.add(new RtYamlLine("# lines", 3));
+        final StringBuilder expected = new StringBuilder();
+        expected
+                .append("comment").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("on multiple").append(System.lineSeparator())
+                .append(System.lineSeparator())
+                .append("lines");
+        MatcherAssert.assertThat(
+                new ReadComment(
+                        lines,
+                        Mockito.mock(YamlNode.class)
+                ).value(),
+                Matchers.equalTo(expected.toString())
+        );
+    }
+
+    /**
+     * ReadComment can return an empty string if the comment
+     * line is empty.
+     */
+    @Test
+    public void returnsValueFromEmptySingleComment() {
+        final List<YamlLine> lines = new ArrayList<>();
+        lines.add(new RtYamlLine("#", 0));
+        MatcherAssert.assertThat(
+            new ReadComment(
+                lines,
+                Mockito.mock(YamlNode.class)
+            ).value(),
+            Matchers.isEmptyString()
+        );
+    }
+
 }
