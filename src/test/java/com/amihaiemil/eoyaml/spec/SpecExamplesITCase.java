4b94fb2d67bbb6b444e42618bc962b81e0e49adf
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2020-05-15 10:11:01 -0300

    Adds more spec example tests

--- src/test/java/com/amihaiemil/eoyaml/spec/SpecExamplesITCase.java
+++ src/test/java/com/amihaiemil/eoyaml/spec/SpecExamplesITCase.java
@@ -41,21 +41,22 @@ import org.junit.Test;
  * @author Aislan Nadrowski (aislan.nadrowski@gmail.com)
  * @version $Id$
  * @since 4.3.6
- * 
- * @todo #238:30min Add more tests to cover all the examples from the Yaml 1.2 
- *  Specification. You can have a look at PR 335 to see how other examples have 
- *  been tested. Don't forget that you do not have to write all the tests in 
- *  this task -- write 1 or 2 tests, leave a todo (puzzle) and the work will 
- *  continue in a future ticket.
+ *
+ * @todo #336:30min Add more tests to cover all the examples from the Yaml 1.2
+ * Specification. You can have a look at PR 335 to see how other examples have
+ * been tested. Don't forget that you do not have to write all the tests in this
+ * task -- write 1 or 2 tests, leave a todo (puzzle) and the work will continue
+ * in a future ticket.
  */
 public final class SpecExamplesITCase {
 
     /**
      * It tests the example 2.1 of the Yaml Spec 1.2.
+     *
      * @throws Exception If something goes wrong
      */
     @Test
-    public void readFirstSpecExample() throws Exception {
+    public void readSpecExampleTwoDotOne() throws Exception {
         final YamlSequence expected = Yaml.createYamlSequenceBuilder()
             .add("Mark McGwire")
             .add("Sammy Sosa")
@@ -70,10 +71,11 @@ public final class SpecExamplesITCase {
 
     /**
      * It tests the example 2.2 of the Yaml Spec 1.2.
+     *
      * @throws Exception If something goes wrong
      */
     @Test
-    public void readSecondSpecExample() throws Exception {
+    public void readSpecExampleTwoDotTwo() throws Exception {
 
         final YamlMapping expected = Yaml.createYamlMappingBuilder()
             .add(
@@ -104,10 +106,11 @@ public final class SpecExamplesITCase {
 
     /**
      * It tests the example 2.3 of the Yaml Spec 1.2.
+     *
      * @throws Exception If something goes wrong
      */
     @Test
-    public void readThirdSpecExample() throws Exception {
+    public void readSpecExampleTwoDotThree() throws Exception {
         final YamlMapping expected = Yaml.createYamlMappingBuilder()
             .add("american",
                 Yaml.createYamlSequenceBuilder()
@@ -131,6 +134,7 @@ public final class SpecExamplesITCase {
 
     /**
      * It tests the example 2.4 of the Yaml Spec 1.2.
+     *
      * @throws Exception If something goes wrong
      */
     @Test
@@ -155,6 +159,66 @@ public final class SpecExamplesITCase {
         ).readYamlSequence();
         MatcherAssert.assertThat(expected, Matchers.equalTo(actual));
     }
-}
 
-    
+    /**
+     * It tests the example 2.19 of the Yaml Spec 1.2.
+     *
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void readSpecExampleTwoDotNineteen() throws Exception {
+        final YamlMapping expected = Yaml.createYamlMappingBuilder()
+            .add("canonical", "12345")
+            .add("decimal", "+12345")
+            .add("octal", "0o14")
+            .add("hexadecimal", "0xC")
+            .build();
+        System.out.println(expected);
+        final YamlMapping actual = Yaml.createYamlInput(
+            new File("src/test/resources/spec/spec_2_19.yml")
+        ).readYamlMapping();
+        MatcherAssert.assertThat(expected, Matchers.equalTo(actual));
+    }
+
+    /**
+     * It tests the example 2.20 of the Yaml Spec 1.2.
+     *
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void readSpecExampleTwoDotTwenty() throws Exception {
+        final YamlMapping expected = Yaml.createYamlMappingBuilder()
+            .add("canonical", "1.23015e+3")
+            .add("exponential", "12.3015e+02")
+            .add("fixed", "1230.15")
+            .add("negative infinity", "-.inf")
+            .add("not a number", ".NaN")
+            .build();
+        System.out.println(expected);
+        final YamlMapping actual = Yaml.createYamlInput(
+            new File("src/test/resources/spec/spec_2_20.yml")
+        ).readYamlMapping();
+        MatcherAssert.assertThat(expected, Matchers.equalTo(actual));
+    }
+
+    /**
+     * It tests the example 2.22 of the Yaml Spec 1.2.
+     *
+     * @throws Exception If something goes wrong
+     */
+    @Test
+    public void readSpecExampleTwoDotTwentyTwo() throws Exception {
+        final YamlMapping expected = Yaml.createYamlMappingBuilder()
+            .add("canonical", "2001-12-15T02:59:43.1Z")
+            .add("iso8601", "2001-12-14t21:59:43.10-05:00")
+            .add("spaced", "2001-12-14 21:59:43.10 -5")
+            .add("date", "2002-12-14")
+            .build();
+        System.out.println(expected);
+        final YamlMapping actual = Yaml.createYamlInput(
+            new File("src/test/resources/spec/spec_2_22.yml")
+        ).readYamlMapping();
+        MatcherAssert.assertThat(expected, Matchers.equalTo(actual));
+    }
+
+}
