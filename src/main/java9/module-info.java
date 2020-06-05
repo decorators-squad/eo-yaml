62fdf0a76998cec2e4ef61c2ae20c787c2e3f5ad
Hasan Demirtaş <utsukushihito@outlook.com>
2020-06-05 16:30:09 +0300

    Update module-info.java

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -1,6 +1,4 @@
 module com.amihaiemil.eoyaml {
-    requires static commons.beanutils;
-
     exports com.amihaiemil.eoyaml;
     exports com.amihaiemil.eoyaml.exceptions;
     exports com.amihaiemil.eoyaml.extensions;
