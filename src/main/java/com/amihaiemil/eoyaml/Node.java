080a3f14a1ab751300c62d04e186cfefbaf66306
Hasan Demirtaş <utsukushihito@outlook.com>
2020-06-10 11:52:52 +0300

    Update Node.java

--- src/main/java/com/amihaiemil/eoyaml/Node.java
+++ src/main/java/com/amihaiemil/eoyaml/Node.java
@@ -1,21 +1,24 @@
 package com.amihaiemil.eoyaml;
 
+/**
+ * The class is for defining the type of {@link YamlNode}.
+ */
 public enum Node {
 
     /**
-     * Type of {@link Scalar}
+     * Type of {@link Scalar}.
      */
     SCALAR,
     /**
-     * Type of {@link YamlMapping}
+     * Type of {@link YamlMapping}.
      */
     MAPPING,
     /**
-     * Type of {@link YamlStream}
+     * Type of {@link YamlStream}.
      */
     STREAM,
     /**
-     * Type of {@link YamlSequence}
+     * Type of {@link YamlSequence}.
      */
     SEQUENCE
 
