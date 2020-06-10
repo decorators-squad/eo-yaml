12d4029a8c52bbe84a604a91f335af79a58d36ae
Hasan Demirtaş <utsukushihito@outlook.com>
2020-06-10 12:27:04 +0300

    old

--- src/main/java/com/amihaiemil/eoyaml/YamlStream.java
+++ src/main/java/com/amihaiemil/eoyaml/YamlStream.java
@@ -3,15 +3,15 @@
  * All rights reserved.
  * <p>
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ *  modification, are permitted provided that the following conditions are met:
  * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
+ *  list of conditions and the following disclaimer.
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
  * Neither the name of the copyright holder nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -37,20 +37,19 @@ import java.util.stream.*;
  * All the methods have a default implementations based on the YamlNode
  * values Collection.
  *
- * @author Mihai Andronache (amihaiemil@gmail.com)
- * @version $Id$
  * @checkstyle FinalParameters (400 lines)
  * @checkstyle JavadocMethod (400 lines)
  * @checkstyle LineLength (400 lines)
  * @checkstyle ParameterName (400 lines)
  * @checkstyle FinalParameters (400 lines)
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
  * @since 3.1.1
  */
 public interface YamlStream extends YamlNode, Stream<YamlNode> {
 
     /**
      * Fetch the values from this stream.
-     *
      * @return Collection of {@link YamlNode}
      */
     Collection<YamlNode> values();
