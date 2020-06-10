ba0c82f3d2f88ef229ca1c015f199ef3672ebef4
Hasan Demirtaş <utsukushihito@outlook.com>
2020-06-10 12:27:45 +0300

    Update YamlStream.java

--- src/main/java/com/amihaiemil/eoyaml/YamlStream.java
+++ src/main/java/com/amihaiemil/eoyaml/YamlStream.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2016-2020, Mihai Emil Andronache
  * All rights reserved.
- * <p>
+ *
  * Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
  * Redistributions of source code must retain the above copyright notice, this
