4a1d6e1ce33e1669fad7f9f39c4cb2bb49b6e401
Hasan Demirtaş <utsukushihito@outlook.com>
2020-07-17 18:40:31 +0300

    Update BenchmarkRunner.java

--- src/test/java/com/amihaiemil/eoyaml/benchmark/BenchmarkRunner.java
+++ src/test/java/com/amihaiemil/eoyaml/benchmark/BenchmarkRunner.java
@@ -2,19 +2,21 @@ package com.amihaiemil.eoyaml.benchmark;
 
 import com.amihaiemil.eoyaml.Yaml;
 import com.esotericsoftware.yamlbeans.YamlReader;
+import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.*;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.Main;
 import org.openjdk.jmh.annotations.*;
 
-@BenchmarkMode(Mode.AverageTime)
+@BenchmarkMode(Mode.All)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
 @Fork(value = 1, jvmArgs = {"-Xms2G", "-Xmx2G"})
-@Warmup(iterations = 3)
-@Measurement(iterations = 3)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
 public class BenchmarkRunner {
 
     public static void main(final String[] args) throws IOException {
@@ -26,37 +28,45 @@ public class BenchmarkRunner {
     }
 
     @Benchmark
-    public void eoyaml() throws IOException {
+    public void eoyamlLoadAndGetString() throws IOException {
         try (final InputStream stream =
                  new FileInputStream(new File("target/test-classes/benchmark", "benchmark.yml"))) {
             Yaml.createYamlInput(stream, true)
-                .readYamlMapping();
+                .readYamlMapping()
+                .yamlMapping("run")
+                .string("benchmarkName");
         }
     }
 
     @Benchmark
-    public void snakeyaml() throws IOException {
+    public void snakeyamlLoadAndGetString() throws IOException {
         try (final Reader reader =
                  new FileReader(new File("target/test-classes/benchmark", "benchmark.yml"))) {
-            new org.yaml.snakeyaml.Yaml().load(reader);
+            final Map<Object, Object> map = new org.yaml.snakeyaml.Yaml().load(reader);
+            final Map<Object, Object> run = (Map<Object, Object>) map.get("run");
+            run.get("benchmarkName");
         }
     }
 
     @Benchmark
-    public void yamlbeans() throws IOException {
+    public void yamlbeansLoadAndGetString() throws IOException {
         try (final Reader reader =
                  new FileReader(new File("target/test-classes/benchmark", "benchmark.yml"))) {
-            new YamlReader(reader).read();
+            final Map<Object, Object> map = new YamlReader(reader).read(Map.class);
+            final Map<Object, Object> run = (Map<Object, Object>) map.get("run");
+            run.get("benchmarkName");
         }
     }
 
     @Benchmark
-    public void jacksonyaml() throws IOException {
+    public void jacksonyamlLoadAndGetString() throws IOException {
         try (final Reader reader =
                  new FileReader(new File("target/test-classes/benchmark", "benchmark.yml"))) {
-            new ObjectMapper(new YAMLFactory())
+            final TreeNode node = new ObjectMapper(new YAMLFactory())
                 .createParser(reader)
                 .readValueAsTree();
+            final TreeNode run = node.get("run");
+            run.get("benchmarkName");
         }
     }
 
