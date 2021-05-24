acb32cfa66a2a0cdf50270d52a9a72187a480f51
Mihai <amihaiemil@gmail.com>
2021-05-24 17:14:26 +0300

    Update rrv.sh

--- rrv.sh
+++ rrv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Rultor release versioning script for Maven projects.
 #
+#
 # It looks for the project’s version, which MUST respect the pattern 
 # [0-9]*\.[0-9]*\.[0-9]*-SNAPSHOT and BE THE FIRST MATCH in pom.xml
 #
