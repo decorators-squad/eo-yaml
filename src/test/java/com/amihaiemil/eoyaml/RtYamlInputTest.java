decorators-squad/eo-yaml

commit 09795e3e5693827ab68b80bd580314fa15b1c403

#447 Mapping is on same line with sequence marker.

src/main/java/com/amihaiemil/eoyaml/RtYamlInput.java
@@ -34,6 +34,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Implementation for {@link YamlInput}. "Rt" stands for "Runtime".
@@ -133,13 +134,66 @@ final class RtYamlInput implements YamlInput {
             String line;
             int number = 0;
             while ((line = reader.readLine()) != null) {
-                final YamlLine current = new RtYamlLine(line, number);
-                if(!current.toString().trim().isEmpty()) {
-                    lines.add(current);
+
+                if (this.mappingStartsAtDash(line)) {
+
+                    // if line starts with a sequence ("-") and the first
+                    // key:value is unescaped and on the same line with the
+                    // sequence marker, then split the line by keeping the "-"
+                    // on the same indentation and move the key:value on the
+                    // next line with correct indentation relative to "-".
+                    // see bug:
+                    // https://github.com/decorators-squad/eo-yaml/issues/447
+
+                    final String seqIndent = Stream.iterate(" ", s -> s)
+                        .limit(new RtYamlLine(line, number).indentation())
+                        .reduce((acc, space) -> acc + space)
+                        .orElse("");
+                    final YamlLine sequenceLine = new RtYamlLine(
+                        seqIndent + "-",
+                        number
+                    );
+                    lines.add(sequenceLine);
+
+                    // 2 spaces offset
+                    final String offset = "  ";
+                    final String keyValueIndent = seqIndent + offset;
+                    final YamlLine keyValueLine = new RtYamlLine(
+                        keyValueIndent + line.split("-")[1].trim(),
+                        ++number
+                    );
+                    if (!keyValueLine.toString().trim().isEmpty()) {
+                        lines.add(keyValueLine);
+                    }
+                } else {
+                    final YamlLine current = new RtYamlLine(line, number);
+                    if (!current.toString().trim().isEmpty()) {
+                        lines.add(current);
+                    }
                 }
                 number++;
             }
         }
         return new AllYamlLines(lines);
+    }
+
+    /**
+     * Is the <i>key:value</i> on the same line as the same sequence marker
+     * <i>-</i> ?.
+     * <br/>
+     * Example:
+     * <br/>
+     * <code>
+     *     - foo: bar
+     * </code>
+     * @param line Line.
+     * @return Boolean.
+     */
+    private boolean mappingStartsAtDash(final String line){
+        //line without indentation.
+        final String trimmed = line.trim();
+        final boolean escapedScalar = trimmed.matches("^[ ]*-[ ]*\".*\"$")
+            || trimmed.matches("^[ ]*-[ ]*'.*'$");
+        return trimmed.matches("^[ ]*-.*:.+$") && !escapedScalar;
     }
 }

src/test/java/com/amihaiemil/eoyaml/RtYamlInputTest.java
@@ -775,6 +775,54 @@ public final class RtYamlInputTest {
     }
 
     /**
+     * Corner case when key:value is on the same with sequence marker.
+     * <a href="https://github.com/decorators-squad/eo-yaml/issues/447">#447</a>
+     * @throws IOException If something is wrong.
+     */
+    @Test
+    public void shouldReadSequenceWhenFirstKeyIsOnTheSameLineFirstCase()
+        throws IOException {
+        final String fileName = "src/test/resources/issue_447_bug_mapping"
+            + "_case_1.yml";
+        final YamlMapping root = Yaml.createYamlInput(new File(fileName))
+            .readYamlMapping();
+        final YamlSequence variables = root.yamlSequence("root");
+        MatcherAssert.assertThat(variables, Matchers.iterableWithSize(3));
+    }
+
+    /**
+     * Corner case when key:value is on the same with sequence marker.
+     * <a href="https://github.com/decorators-squad/eo-yaml/issues/447">#447</a>
+     * @throws IOException If something is wrong.
+     */
+    @Test
+    public void shouldReadSequenceWhenFirstKeyIsOnTheSameLineSecondCase()
+        throws IOException {
+        final String fileName = "src/test/resources/issue_447_bug_mapping"
+            + "_case_2.yml";
+        final YamlMapping root = Yaml.createYamlInput(new File(fileName))
+            .readYamlMapping();
+        final YamlSequence variables = root.yamlSequence("root");
+        MatcherAssert.assertThat(variables, Matchers.iterableWithSize(3));
+    }
+
+    /**
+     * Corner case when key:value is on the same with sequence marker.
+     * <a href="https://github.com/decorators-squad/eo-yaml/issues/447">#447</a>
+     * @throws IOException If something is wrong.
+     */
+    @Test
+    public void shouldReadSequenceWhenFirstKeyIsOnTheSameLineThirdCase()
+        throws IOException {
+        final String fileName = "src/test/resources/issue_447_bug_mapping"
+            + "_case_3.yml";
+        final YamlMapping root = Yaml.createYamlInput(new File(fileName))
+            .readYamlMapping();
+        final YamlSequence variables = root.yamlSequence("root");
+        MatcherAssert.assertThat(variables, Matchers.iterableWithSize(3));
+    }
+
+    /**
      * Read a test resource file's contents.
      * @param fileName File to read.
      * @return File's contents as String.

src/test/resources/issue_447_bug_mapping_case_1.yml
@@ -1,0 +1,6 @@
+root:
+  - string1
+  - string2
+  - string3
+
+

src/test/resources/issue_447_bug_mapping_case_2.yml
@@ -1,0 +1,7 @@
+root:
+  - key: key1
+    value: value1
+  - key: key2
+    value: value2
+  - key: key3
+    value: value3

src/test/resources/issue_447_bug_mapping_case_3.yml
@@ -1,0 +1,7 @@
+root:
+  - string1
+  - key: key1
+    value: value1
+  - key: key2
+    value: value2
+

commit cf105bc9e4fd9ce5eb1e9ea711d72490c5204571

Fix YamlReadingException when first key of a  mapping in a compact sequence contains a dash
The bug was introduced in 09795e3

src/main/java/com/amihaiemil/eoyaml/RtYamlInput.java
@@ -160,7 +160,7 @@ final class RtYamlInput implements YamlInput {
                     final String offset = "  ";
                     final String keyValueIndent = seqIndent + offset;
                     final YamlLine keyValueLine = new RtYamlLine(
-                        keyValueIndent + line.split("-")[1].trim(),
+                        keyValueIndent + line.split("-", 2)[1].trim(),
                         ++number
                     );
                     if (!keyValueLine.toString().trim().isEmpty()) {

src/test/java/com/amihaiemil/eoyaml/RtYamlInputTest.java
@@ -879,6 +879,27 @@ public final class RtYamlInputTest {
         );
     }
 
+    /**
+     * Corner case when key:value is on the same line as the sequence marker
+     * and key contains dashes (which is the sequence marker itself).
+     *  @throws IOException If something is wrong.
+     */
+    @Test
+    public void shouldReadSequenceWhenFirstKeyWithDashesIsOnTheSameLine()
+        throws IOException {
+        final String fileName = "src/test/resources/"
+            + "compactSequenceWithDashedKey.yml";
+        final YamlMapping root = Yaml.createYamlInput(new File(fileName))
+            .readYamlMapping();
+
+        final YamlSequence sequence = root.yamlSequence("root");
+        MatcherAssert.assertThat(sequence, Matchers.iterableWithSize(2));
+
+        MatcherAssert.assertThat(
+            sequence.yamlMapping(0).string("a-a-a"),
+            Matchers.equalTo("1")
+        );
+    }
 
     /**
      * Do a round-trip test on the bracketed-key sample file.

src/test/resources/compactSequenceWithDashedKey.yml
@@ -1,0 +1,5 @@
+root:
+  - a-a-a: 1
+    b: 2
+  - a: 3
+    b: 4