468e8b3e8144cbd3ee8851d18f3686eb40bb807d
amihaiemil <amihaiemil@gmail.com>
2022-11-26 16:34:53 +0200

    update licence headers to 2023

--- src/main/java/com/amihaiemil/eoyaml/Concatenated.java
+++ src/main/java/com/amihaiemil/eoyaml/Concatenated.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016-2021, Mihai Emil Andronache
+ * Copyright (c) 2016-2023, Mihai Emil Andronache
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
